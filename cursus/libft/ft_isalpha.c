42755377fef85cfdf09031f69773387424cae42a
RaphyStoll <raphael.ferreira@ik.me>
2023-11-10 22:37:19 +0100

    update

---
+++ cursus/libft/ft_isalpha.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "libft.h"
+
+int ft_isalpha(int c)
+{
+    return(('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'));
+}
+/*
+int main()
+{
+    printf("a = return %d\n", ft_isalpha('a'));
+    printf("A = return %d\n", ft_isalpha('A'));
+    printf("1 = return %d\n", ft_isalpha('1'));
+    return 0;
+}*/
--- cursus/libft/libft.h
+++ cursus/libft/libft.h
@@ -8,5 +8,7 @@
 //fonctions libc
 void *ft_memset(void *s, int c, size_t len);
 void *ft_bzero(void *s, size_t num);
+void *memcpy(void *dest, const void * src, size_t n);
+int ft_isalpha(int c);
 
 #endif
